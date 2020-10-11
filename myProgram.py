bd9f40e598102d4cba89b4174e10832d982f08cb
Daphne Witmer <daphne.witmer@experius.nl>
2020-10-11 19:16:29 +0200

    Make signature function random

--- myProgram.py
+++ myProgram.py
@@ -1,10 +1,11 @@
 import os
+import random
 from owlready2 import *
 
 # This is an example python programme which shows how to use the different stand-alone versions of OWL reasoners and forgetting programme
 
 # Choose the ontology (in the OWL format) for which you want to explain the entailed subsumption relations.
-#inputOntology = "datasets/pizza.owl"
+# inputOntology = "datasets/animals.owl"
 
 # 1. PRINT ALL SUBCLASSES (inputOntology):
 # print all subClass statements (explicit and inferred) in the inputOntology
@@ -18,7 +19,7 @@ from owlready2 import *
 
 # Choose the set of subclass for which you want to find an explanation.
 # this file can be generated using the second command (saveAllSubClasses)
-#inputSubclassStatements = "datasets/subClasses.nt"
+# inputSubclassStatements = "datasets/subClasses.nt"
 
 # 3. PRINT ALL EXPLANATIONS (inputOntology, inputSubclassStatements):
 # print explanations for each subClass statement in the inputSubclassStatements
@@ -70,14 +71,18 @@ def get_signature(inputOntology, inputSubclassStatements):
     sigContent = False
 
     # pick random class that is not an input subclass
-    for item in ontoClasses:
+    while True:
+        item = random.choice(ontoClasses)
         signatureOption = base_uri + str(item).lstrip('pizza.')
+
         if signatureOption in subclassStatements:
-            continue
+            ontoClasses.remove(item)
+            if not ontoClasses:
+                break
         else:
             sigContent = signatureOption
+            break
 
-    print(sigContent)
     # Write signature file
     f = open(signature, "a")
     f.truncate(0)
@@ -87,32 +92,68 @@ def get_signature(inputOntology, inputSubclassStatements):
 
     return signature
 
-
-# inputOntology = total ontology, like 'exp10-1.omn'
-# inputSubclassStatements = subclass statement(s) for which you want an explanation, often just one.
-def explain_by_forgetting(inputOntology, inputSubclassStatements, method):
+def explain_by_forgetting(inputOntology, inputSubclassStatement, method):
+    ontologyName = inputOntology # is datasets/justifications/exp1-1_gen_0.omn
 
     # Transform .omn file to .owl file using LETHE and emtpy signature
+    print("TRANSFORMING .omn TO .owl FILE USING LETHE AND EMPTY SIGNATURE")
     signature = "datasets/empty_signature.txt"
     os.system('java -cp lethe-standalone.jar uk.ac.man.cs.lethe.internal.application.ForgettingConsoleApplication --owlFile ' + inputOntology + ' --method ' + method  + ' --signature ' + signature)
-    inputOntology = "result.owl"
+
+    # replace .omn by .owl file for first gereration (..._gen_0.owl)
+    os.remove(inputOntology) # remove ..._gen_0.owl
+    os.rename("result.owl",inputOntology) # change result.owl to datasets/justifications/exp1-1_gen_0.omn
 
     i = 1
-    while True:
-        print('-----------loop', i)
-        # Get a signature
-        signature = get_signature(inputOntology, inputSubclassStatements)
-        if os.stat(signature).st_size == 0: # if signature is emtpy
+    while (True):
+
+        # Get a new signature
+        signature = get_signature(inputOntology, inputSubclassStatement)
+
+        # if signature is emtpy, end function
+        if (os.stat("datasets/signature.txt").st_size == 0):
             break
+
         # Forget:
         os.system('java -cp lethe-standalone.jar uk.ac.man.cs.lethe.internal.application.ForgettingConsoleApplication --owlFile ' + inputOntology + ' --method ' + method + ' --signature ' + signature)
-        # set the resulting ontology as the new ontology
-        inputOntology = "forget_gen_" + str(i) + ".owl"
-        # and save the result as forget_gen_i.owl
-        os.rename("result.owl","forget_gen_" + str(i) + ".owl")
+
+        # save the result as forget_gen_{i}.owl
+        os.rename("result.owl", str(ontologyName.replace("_0","_" + str(i))))
+
+        # set new file as new inputOntology, repeat forgetting
+        inputOntology = str(ontologyName.replace("_0","_" + str(i)))
         i+= 1
 
+def get_subsets_justifications_forgetting(inputOntology):
+    # first save all subsets
+    os.system('java -jar kr_functions.jar ' + 'saveAllSubClasses' + " " + inputOntology)
 
-ont = "datasets/exp1.omn"
-subset = "datasets/subclass1.nt"
-explain_by_forgetting(ont, subset, "2")
+    # then save all subsets in a separate file called "Subset_{number}"
+    subclasses = open("datasets/subClasses.nt")
+    lines = subclasses.readlines()
+    file_count = 1
+    # for every subset
+    for line in lines:
+        if line.strip():
+            # save as individual subset
+            with open('datasets/subsets/Subset_{}.nt'.format(file_count), 'w') as outfile:
+                outfile.write(line)
+
+        # save all explanations for this subset
+        fileName = 'datasets/subsets/Subset_' + str(file_count) + ".nt"
+        os.system('java -jar kr_functions.jar ' + 'saveAllExplanations' + " " + inputOntology + " " + fileName)
+
+        # for every explanation, rename and add it to justifications folder
+        for file in os.listdir("datasets"):
+            if file.startswith("exp"):
+                newName = "datasets/justifications/exp" + str(file_count) + "-" + str(file[5]) + "_gen_0.owl"
+                oldName = "datasets/" + str(file)
+                os.rename(oldName,newName)
+
+                # als de explanation groter is dan... !!!!!!!!!!!!!!!!!
+                explain_by_forgetting(newName, fileName, "2")
+
+        file_count += 1
+
+inputOntology = "datasets/pizza_super_simple.owl"
+get_subsets_justifications_forgetting(inputOntology)
